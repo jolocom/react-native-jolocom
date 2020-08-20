289dc279d2c4a32e0cff3d415d355ee79a87694c
mnzaki <mnzaki@gmail.com>
2020-08-20 13:14:57 +0200

    chore: update dist files

--- js/transports.d.ts
+++ js/transports.d.ts
@@ -1,9 +1,26 @@
+import { ChannelTransportAPI, ChannelTransport } from '@jolocom/sdk/js/src/lib/channels';
 import { JolocomSDK, JolocomPlugin } from '@jolocom/sdk';
 import { InteractionTransport, InteractionTransportAPI } from '@jolocom/sdk/js/src/lib/interactionManager/interactionManager';
+export declare class JolocomWebSockets implements JolocomPlugin {
+    register(sdk: JolocomSDK): Promise<void>;
+    startChannelTransport(transport: ChannelTransport): WebSocketsChannelTransport;
+}
+declare class WebSocketsChannelTransport implements ChannelTransportAPI {
+    private ws;
+    private _receivePromise;
+    private _resolveReceive?;
+    ready: Promise<void>;
+    constructor(transport: ChannelTransport);
+    send(msg: string): void;
+    onMessage(msg: string): void;
+    receive(): Promise<string>;
+    stop(): void;
+}
 export declare class JolocomLinking implements JolocomPlugin {
     register(sdk: JolocomSDK): Promise<void>;
-    startTransport(config: InteractionTransport): InteractionTransportAPI;
+    startTransport(transport: InteractionTransport): InteractionTransportAPI;
 }
 export declare enum ErrorCode {
-    DeepLinkCannotBeOpened = "DeepLinkCannotBeOpened"
+    LinkCannotBeOpened = "LinkCannotBeOpened"
 }
+export {};
