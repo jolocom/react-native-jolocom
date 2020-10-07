5c5ebde3d4aa35f9917db151d57b763f374fbabf
mnzaki <mnzaki@gmail.com>
2020-10-07 11:22:52 +0200

    feat: update to SDK API 1.0
    - refactor transpors, bulk of which is now in SDK
    - update IPasswordStore usage

--- js/passwordStore.js
+++ js/passwordStore.js
@@ -3,23 +3,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.JolocomKeychainPasswordStore = void 0;
 const tslib_1 = require("tslib");
 const Keychain = tslib_1.__importStar(require("react-native-keychain"));
+const randomBytes = require('react-native-randomBytes');
+/**
+ * This PasswordStore will use the Android/iOS native secure storage to store a
+ * randomly generated 32byte password
+ */
 class JolocomKeychainPasswordStore {
     constructor() {
+        this.service = 'jolocom';
         this.username = 'JolocomSmartWallet';
         this.nativeLib = Keychain;
     }
-    savePassword(password) {
+    setPassword(password) {
         return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.nativeLib.setGenericPassword(this.username, password);
+            yield this.nativeLib.setGenericPassword(this.username, password, {
+                service: this.service,
+                storage: Keychain.STORAGE_TYPE.AES,
+            });
         });
     }
     getPassword() {
         return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            const result = yield this.nativeLib.getGenericPassword();
-            if (typeof result === 'boolean') {
-                throw new Error('Password could not be retrieved from the keychain');
+            const result = yield this.nativeLib.getGenericPassword({
+                service: this.service
+            });
+            if (result === false) {
+                // there is no password stored
+                const password = yield new Promise((res, rej) => {
+                    randomBytes(32, (err, bytes) => {
+                        if (err)
+                            return rej(err);
+                        res(bytes);
+                    });
+                });
+                yield this.setPassword(password);
+                return password;
             }
-            return result.password;
+            else {
+                return result.password;
+            }
         });
     }
 }
--- ts/passwordStore.ts
+++ ts/passwordStore.ts
@@ -1,21 +1,40 @@
 import * as Keychain from 'react-native-keychain'
-import { IPasswordStore } from '@jolocom/sdk/js/src/lib/storage'
+import { IPasswordStore } from '@jolocom/sdk/js/storage'
 
+const randomBytes = require('react-native-randomBytes')
+/**
+ * This PasswordStore will use the Android/iOS native secure storage to store a
+ * randomly generated 32byte password
+ */
 export class JolocomKeychainPasswordStore implements IPasswordStore {
+  private service = 'jolocom'
   private username = 'JolocomSmartWallet'
   private nativeLib = Keychain
 
-  async savePassword(password: string): Promise<void> {
-    await this.nativeLib.setGenericPassword(this.username, password)
+  private async setPassword(password: string): Promise<void> {
+    await this.nativeLib.setGenericPassword(this.username, password, {
+      service: this.service,
+      storage: Keychain.STORAGE_TYPE.AES,
+    })
   }
 
   async getPassword(): Promise<string> {
-    const result = await this.nativeLib.getGenericPassword()
+    const result = await this.nativeLib.getGenericPassword({
+      service: this.service
+    })
 
-    if (typeof result === 'boolean') {
-      throw new Error('Password could not be retrieved from the keychain')
+    if (result === false) {
+      // there is no password stored
+      const password = await new Promise<string>((res, rej) => {
+        randomBytes(32, (err: Error, bytes: string) => {
+          if (err) return rej(err)
+          res(bytes)
+        })
+      })
+      await this.setPassword(password)
+      return password
+    } else {
+      return result.password
     }
-
-    return result.password
   }
 }
